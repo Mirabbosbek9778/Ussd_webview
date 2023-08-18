ca7ecb6d492614ea28f3134f1ed0481cc6f44df8
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2023-08-18 11:35:27 +0500

    card edit

--- src/components/Generic/cards/Category-card/index.jsx
+++ src/components/Generic/cards/Category-card/index.jsx
@@ -3,10 +3,10 @@ import { CategoryCardArrow } from "../../../iconsStyle/index.js";
 export default function CategoryCard({ icon, title }) {
   return (
     <Fragment>
-      <div className=" w-11/12 flex items-center my-5 h-[90px] bg-[var(--card-color)] rounded-xl justify-around shadow">
+      <div className=" w-11/12 flex items-center my-5 cursor-pointer select-none py-4 px-[14px]  h-[61px] bg-[var(--card-color)] rounded-xl justify-around shadow">
         <div className="w-[200px] flex items-center justify-between">
           <img className="w-[40px]" src={icon} alt="icon" />
-          <p className="font-bold text-xl">{title}</p>
+          <p className="font-bold text-[16px] text-[var(--text-color)] ">{title}</p>
         </div>
         <CategoryCardArrow />
       </div>
