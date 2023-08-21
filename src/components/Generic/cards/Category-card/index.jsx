b624d91cb5c8f3035722617b98f0c72e02c06283
sanjardev1 <sanjarbekqobulov02gmail.com>
2023-08-21 21:37:10 +0500

    fixed bugs in main pages

--- src/components/Generic/cards/Category-card/index.jsx
+++ src/components/Generic/cards/Category-card/index.jsx
@@ -1,12 +1,12 @@
-import { Fragment } from "react";
-import { CategoryCardArrow } from "../../../iconsStyle/index.js";
+import { Fragment } from 'react';
+import { CategoryCardArrow } from '../../../iconsStyle/index.js';
 export default function CategoryCard({ icon, title }) {
   return (
     <Fragment>
-      <div className="category-card">
-        <div className="flex items-center justify-between">
-          <img className="w-[30px] h-[30px]" src={icon} alt="icon" />
-          <p className="font-bold text-[var(--text-width)] ml-[8px] text-[var(--text-color)] ">
+      <div className='category-card'>
+        <div className='flex items-center justify-between'>
+          <img className='w-[24px] h-[24px]' src={icon} alt='icon' />
+          <p className='font-bold text-[var(--text-width)] ml-[8px] text-[var(--text-color)] '>
             {title}
           </p>
         </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,22 @@
-import Carosel from "../../components/Carusel";
-import { category } from "../../mock/category";
-import CategoryCard from "../../components/Generic/cards/Category-card";
-import Navbar from "../../components/Navbar";
-import TopDriver from "../../components/driver/Top-driver";
+import Carosel from '../../components/Carusel';
+import { category } from '../../mock/category';
+import CategoryCard from '../../components/Generic/cards/Category-card';
+import Navbar from '../../components/Navbar';
+import TopDriver from '../../components/driver/Top-driver';
 const index = () => {
   return (
-    <div className="w-full">
-      <TopDriver />
-      <Carosel />
-      <div className="category-card-layout">
+    <div className='w-full'>
+      <div className='fixed top-0 left-0 right-0 max-w-[700px] min-w-[375px] mx-auto text-center'>
+        <TopDriver />
+        <Carosel />
+      </div>
+      <div className='h-[234px]'></div>
+      <div className='category-card-layout'>
         {category?.map((el) => (
           <CategoryCard key={el.id} icon={el.icon} title={el.name} />
         ))}
       </div>
-
-      <div className="w-full absolute bottom-0 left-0 right-0 z-50">
-        <Navbar />
-      </div>
+      <Navbar />
     </div>
   );
 };
