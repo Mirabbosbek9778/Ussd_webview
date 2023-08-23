8012edf18e6434549ff7438e0320047298bde427
sanjardev1 <sanjarbekqobulov02gmail.com>
2023-08-23 11:01:30 +0500

    bugs fixed

--- src/pages/InternetPakets/index.jsx
+++ src/pages/InternetPakets/index.jsx
@@ -7,7 +7,7 @@ const index = () => {
   return (
     <div className='bg-[var(--bg-color)] h-[100%] flex flex-col  '>
       <div className='h-[89px]'></div>
-      <TopBar />
+      <TopBar title={'Internet paketlar'} />
       <InternetCards type={internet[0].name} data={internet[0].pakets} />
       <div className='mt-4'>
         <InternetCards
