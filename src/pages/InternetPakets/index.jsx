2f2b44bdb06adf68eda9c6763bc64d17b2fdd630
sanjardev1 <sanjarbekqobulov02gmail.com>
2023-08-23 11:20:31 +0500

    bugs fixed

--- src/pages/InternetPakets/index.jsx
+++ src/pages/InternetPakets/index.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import TopBar from '../../components/TopBar';
-import { internet, internetDaily } from '../../mock/category';
+import { internet } from '../../mock/category';
 
 import InternetCards from '../../components/Generic/cards/InternetCARDLAR/InternetCards';
 const index = () => {
@@ -8,13 +8,13 @@ const index = () => {
     <div className='bg-[var(--bg-color)] h-[100%] flex flex-col  '>
       <div className='h-[89px]'></div>
       <TopBar title={'Internet paketlar'} />
-      <InternetCards type={internet[0].name} data={internet[0].pakets} />
+      {/* <InternetCards type={internet[0].name} data={internet[0].pakets} />
       <div className='mt-4'>
         <InternetCards
           type={internetDaily[0].name}
           data={internetDaily[0].pakets}
         />
-      </div>
+      </div> */}
     </div>
   );
 };
