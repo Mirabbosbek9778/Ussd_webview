d04fce70a9f7d134d680b8c8d2e2ae10a7a5aa2c
Mirabbosbek9778 <jwicj969@gmail.com>
2023-08-26 11:56:36 +0500

    modal active button

--- src/components/singleCard/index.jsx
+++ src/components/singleCard/index.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-import { BtnActive, Description, TitleText } from "./style";
+import React, { useState } from "react";
+import {
+  Active,
+  BtnActive,
+  BtnModal,
+  BtnP,
+  Cancel,
+  Description,
+  TitleText,
+} from "./style";
+import { Modal } from "antd";
 
 const SingleCard = ({ description }) => {
+  const [modal, contextHolder] = Modal.useModal();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   return (
     <div>
       <TitleText className="">Tavsif</TitleText>
@@ -9,10 +32,44 @@ const SingleCard = ({ description }) => {
         <p>{description}</p>
       </Description>
       <BtnActive>
-        <button>Paketni faollashtirish</button>
+        <button className="text-withe" onClick={showModal}>
+          Paketni faollashtirish
+          {contextHolder}
+        </button>
       </BtnActive>
+
+      <Modal
+        title={
+          <div className="flex flex-col items-center justify-center">
+            <div>
+              <BtnModal className="">Paketni faollashtirish</BtnModal>
+            </div>
+            <BtnP className="text-center">
+              Siz “Barakali 22” tarifini <p>faollashtirmoqchisiz</p>
+              O’tish narxi: 22 000 so’m
+            </BtnP>
+          </div>
+        }
+        open={isModalVisible}
+        footer={null}
+        closable={false}
+        style={{
+          position: "absolute",
+          top: "40%",
+          left: "0",
+          bottom: "0",
+          right: "0",
+          padding: "0 !important", // Set padding to 0
+        }}
+        maskStyle={{ background: "rgba(0, 0, 0, 0.25)" }}
+      >
+        <div className="flex w-full mx-auto border rounded-lg justify-evenly h-11">
+          <Cancel onClick={handleCancel}>Bekor qilish</Cancel>
+          <div className="border"></div>
+          <Active onClick={handleOk}>Faollashtirish</Active>
+        </div>
+      </Modal>
     </div>
   );
 };
-
 export default SingleCard;
--- src/components/singleCard/style.js
+++ src/components/singleCard/style.js
@@ -22,7 +22,6 @@ export const Description = styled.div`
   padding-top: 10px;
   margin: 16px;
   padding: 16px 16px 16px 16px;
-
 `;
 
 export const BtnActive = styled.div`
@@ -31,11 +30,51 @@ export const BtnActive = styled.div`
   border: 1px solid red gap;
   margin-top: 16px;
   margin: 16px 16px 16px 16px;
-  
   border-radius: 12px;
   height: 55px;
   flex-direction: column;
   gap: 13px;
   padding-top: 15px;
   color: white;
+`;
+
+export const BtnModal = styled.div`
+  color: var(--text-color);
+  font-family: Okta Neue, sans-serif;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
+export const BtnP = styled.div`
+  color: black !important;
+  text-align: center;
+  font-family: Okta Neue, sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  padding-top: 10px;
+`;
+
+export const Cancel = styled.button`
+  color: #fa2d2d;
+  text-align: center;
+  font-family: Okta Neue, sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  
+`;
+
+export const Active = styled.button`
+  color: var(--ussd-color);
+  text-align: center;
+  font-family: Okta Neue, sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `;
