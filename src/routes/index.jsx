653781b0fd3dee7926a7dc97aace5d802cac2415
Your Name <you@example.com>
2023-08-29 16:53:02 +0500

    reviewed

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,20 @@
-import { Routes as BrowserRoutes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import USSD from "../pages/USSD";
-import Tarif from "../pages/TarifPage";
-import InternetPakets from "../pages/InternetPakets/index";
-import CallCenter from "../pages/CallCenter";
-import Services from "../pages/Services";
-import SingleServiceP from "../pages/Services/SingleServiceP";
-import InternetFullpackets from "../pages/InternetPakets/Packets/FullPackets";
-import InternetDetails from "../pages/InternetPakets/Packets/PacketDetail";
-import TariffDetail from "../pages/TarifPage/Details";
-import SingleCard from "../components/Description";
+import { Routes as BrowserRoutes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import USSD from '../pages/USSD';
+import Tarif from '../pages/TarifPage';
+import InternetPakets from '../pages/InternetPakets/index';
+import CallCenter from '../pages/CallCenter';
+import Services from '../pages/Services';
+import SingleServiceP from '../pages/Services/SingleServiceP';
+import InternetFullpackets from '../pages/InternetPakets/Packets/FullPackets';
+import InternetDetails from '../pages/InternetPakets/Packets/PacketDetail';
+import TariffDetail from '../pages/TarifPage/Details';
+import SingleCard from '../components/Description';
 export const Routes = () => (
   <BrowserRoutes>
-    <Route path="/" element={<Home />} />
+    <Route path='/' element={<Home />} />
     <Route
-      path="/discount"
+      path='/discount'
       element={
         <>
           <h1>acsiyalar</h1>
@@ -22,29 +22,29 @@ export const Routes = () => (
       }
     />
 
-    <Route path="/ussd" element={<USSD />} />
-    <Route path="/tarif" element={<Tarif />} />
+    <Route path='/ussd' element={<USSD />} />
+    <Route path='/tarif' element={<Tarif />} />
     <Route
-      path="/plans"
+      path='/plans'
       element={
         <>
           <h1>plans</h1>
         </>
       }
     />
-    <Route path="/internet" element={<InternetPakets />} />
-    <Route path="/services" element={<Services />} />
+    <Route path='/internet' element={<InternetPakets />} />
+    <Route path='/services' element={<Services />} />
     <Route
-      path="/sms"
+      path='/sms'
       element={
         <>
           <h1>sms</h1>
         </>
       }
     />
-    <Route path="/call-center" element={<CallCenter />} />
+    <Route path='/call-center' element={<CallCenter />} />
     <Route
-      path="/restart"
+      path='/restart'
       element={
         <>
           <h1>restart</h1>
@@ -52,11 +52,11 @@ export const Routes = () => (
       }
     />
 
-    <Route path="/" element={<Home />} />
-    <Route path="/discount" element={<h1>acsiyalar</h1>} />
-    <Route path="/single-cart" element={<SingleCard />} />
+    <Route path='/' element={<Home />} />
+    <Route path='/discount' element={<h1>acsiyalar</h1>} />
+    <Route path='/single-cart' element={<SingleCard />} />
     <Route
-      path="/settings"
+      path='/settings'
       element={
         <>
           <h1>settings</h1>
@@ -64,18 +64,18 @@ export const Routes = () => (
       }
     />
     <Route
-      path="/plans"
+      path='/plans'
       element={
         <>
           <h1>plans</h1>
         </>
       }
     />
-    <Route path="/internet" element={<InternetPakets />} />
-    <Route path="/internet/full-packets" element={<InternetFullpackets />} />
-    <Route path="/internet/:id" element={<InternetDetails />} />
+    <Route path='/internet' element={<InternetPakets />} />
+    <Route path='/internet/full-packets' element={<InternetFullpackets />} />
+    <Route path='/internet/:id' element={<InternetDetails />} />
     <Route
-      path="/services"
+      path='/services'
       element={
         <>
           <h1>services</h1>
@@ -83,7 +83,7 @@ export const Routes = () => (
       }
     />
     <Route
-      path="/sms"
+      path='/sms'
       element={
         <>
           <h1>sms</h1>
@@ -91,22 +91,14 @@ export const Routes = () => (
       }
     />
     <Route
-      path="/call-center"
+      path='/call-center'
       element={
         <>
           <h1>call-center</h1>
         </>
       }
     />
-    <Route
-      path="/restart"
-      element={
-        <>
-          <h1>restart</h1>
-        </>
-      }
-    />
-    <Route path="/services/service/:id" element={<SingleServiceP />} />
-    <Route path="/tarif/detailed/:id" element={<TariffDetail />} />
+    <Route path='/services/service/:id' element={<SingleServiceP />} />
+    <Route path='/tarif/detailed/:id' element={<TariffDetail />} />
   </BrowserRoutes>
 );
